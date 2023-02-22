d0a517763ce2e6182a937952cbfcbc784250cfbc
hellgateway <yakobusfrans@gmail.com>
2023-02-22 17:59:40 +0700

    kosmetik

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen border border-gray-200 border-r max-w-[250px] p-4">
+  <div class="min-h-screen border border-gray-200 border-r min-w-[150px] p-4">
     <div class="flex flex-col text-gray-400 gap-2 ">
       <nuxt-link class="py-2 capitalize hover:bg-gray-100 w-full px-3 rounded-lg" :class="{ 'text-blue-500 font-bold' : $route.path === '/'}" to="/">show</nuxt-link>
       <nuxt-link class="py-2 capitalize hover:bg-gray-100 w-full px-3 rounded-lg" :class="{ 'text-blue-500 font-bold' : $route.path === '/create'}" to="/create">create</nuxt-link>
